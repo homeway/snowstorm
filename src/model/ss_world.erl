9171deaff1e67e7d019fd058dc5f65781459e972
homeway <homeway.xue@gmail.com>
2015-01-25 01:12:42 +0800

    change dir

--- src/model/ss_world.erl
+++ src/model/ss_world.erl
@@ -7,7 +7,6 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE).
--record(state, {}).
 
 list() -> [ss_user].
 list(live) -> [ss_user].
@@ -18,8 +17,17 @@ start_link() -> gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 start() -> gen_server:start({local, ?SERVER}, ?MODULE, [], []).
 stop() -> gen_server:cast(?SERVER, stop).
 
-init([]) -> {ok, #state{}}.
-handle_call(_Msg, _From, State) -> {reply, ok, State}.
+init([]) -> {ok, #{db=>ss_nosqlite}}.
+handle_call({start, Module, Name, Data}, _From, State) ->
+    {ok, Pid} = Module:start(Name, Data),
+    {reply, ok, maps:put(Name, {Pid, Module}, State)};
+handle_call({stop, Name}, _From, State) ->
+    {_Pid, M} = maps:get(Name, State),
+    M:stop(),
+    {reply, ok, maps:remove(Name, State)};
+handle_call(list, _From, State) ->
+    {reply, State, State}.
+
 handle_cast(stop, State) ->  {stop, normal, State}.
 handle_info(undefined_info, State) ->  {noreply, State}.
 terminate(normal, _State) -> ok.
