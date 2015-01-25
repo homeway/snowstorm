bd4ded1a28867bd5bf0e783024c3fc7b75cde7df
homeway <homeway.xue@gmail.com>
2015-01-26 00:04:28 +0800

    完全使用ss_model_sup的数据作为进程管理依据（以防进程被重启后Pid改变）

--- src/model/ss_world.erl
+++ src/model/ss_world.erl
@@ -20,58 +20,60 @@ clear() -> gen_server:call(?SERVER, clear).
 reg(Name, Mod) -> reg(Name, Mod, []).
 reg(Name, Mod, Args) -> gen_server:call(?SERVER, {reg, Name, Mod, Args}).
 unreg(Name) -> gen_server:call(?SERVER, {unreg, Name}).
-send(Name, Msg) -> gen_server:cast(?SERVER, {send, Name, {self(), Msg}}).
-call(Name, Msg) -> gen_server:call(?SERVER, {call, Name, Msg}).
+send(Name, Msg) -> gen_server:call(?SERVER, {send, Name, Msg}).
+call(Name, Req) -> gen_server:call(?SERVER, {call, Name, Req}).
 find(Name) -> gen_server:call(?SERVER, {find, Name}).
 
 %% start the models supervisor after the world start
 init([]) ->
     ss_model_sup:start_link(),
-    NewS = supervisor:which_children(ss_model_sup),
-    {ok, NewS}.
+    {ok, #{}}.
 
 %% reg model process with a uniq name
-handle_call({reg, Name, Mod, Args}, _From, _S) ->
+handle_call({reg, Name, Mod, Args}, _From, S0) ->
     Reply = case ss_model_sup:start_child(Name, Mod, Args) of
         {ok, Pid} -> Pid;
         {error,{already_started, Pid}} -> Pid;
         _ -> error
     end,
-    NewS = supervisor:which_children(ss_model_sup),
-    {reply, Reply, NewS};
-handle_call({unreg, Name}, _From, _S) ->
+    {reply, Reply, S0};
+handle_call({unreg, Name}, _From, S0) ->
     R = supervisor:delete_child(ss_model_sup, Name),
-    NewS = supervisor:which_children(ss_model_sup),
-    {reply, R, NewS};
-handle_call({find, Name}, _From, S) ->
+    {reply, R, S0};
+handle_call({find, Name}, _From, S0) ->
+    S = supervisor:which_children(ss_model_sup),
     case lists:keyfind(Name, 1, S) of
-        false -> {reply, false, S};
-        {Name, Pid, _, _} -> {reply, Pid, S}
+        false -> {reply, false, S0};
+        {Name, Pid, _, _} -> {reply, Pid, S0}
     end;
-handle_call({call, Name, Msg}, _From, S) ->
+handle_call({call, Name, Req}, _From, S0) ->
+    S = supervisor:which_children(ss_model_sup),
     case lists:keyfind(Name, 1, S) of
-        false -> {reply, false, S};
-        {Name, Pid, _, _} -> {reply, gen_server:call(Pid, Msg), S}
+        false -> {reply, false, S0};
+        {Name, Pid, _, _} -> {reply, gen_server:call(Pid, Req), S0}
     end;
-handle_call(all, _From, S) ->
-    {reply, [{Name, Pid} || {Name, Pid, _, _} <- S], S};
-handle_call({info, Name}, _From, S) ->
+handle_call(all, _From, S0) ->
+    S = supervisor:which_children(ss_model_sup),
+    {reply, [{Name, Pid} || {Name, Pid, _, _} <- S], S0};
+handle_call({info, Name}, _From, S0) ->
+    S = supervisor:which_children(ss_model_sup),
     case lists:keyfind(Name, 1, S) of
-        false -> {reply, false, S};
-        {Name, Pid, _, _} -> {reply, erlang:process_info(Pid), S}
+        false -> {reply, false, S0};
+        {Name, Pid, _, _} -> {reply, erlang:process_info(Pid), S0}
     end;
-handle_call(clear, _From, _S) ->
+handle_call(clear, _From, S0) ->
     OldS = supervisor:which_children(ss_model_sup),
     [supervisor:delete_child(ss_model_sup, Name) || {Name, Pid, _, _} <- OldS, Pid =:= undefined],
     NewS = supervisor:which_children(ss_model_sup),
-    {reply, length(OldS) - length(NewS), NewS}.
-
-handle_cast({send, Name, Msg}, S) ->
-    case lists:keyfind(Name, 1, S) of
-        false -> none;
-        {Name, Pid, _, _} -> gen_server:cast(Pid, Msg)
+    {reply, length(OldS) - length(NewS), S0};
+handle_call({send, Name, Msg}, {From, _}, S0) ->
+    S = supervisor:which_children(ss_model_sup),
+    R = case lists:keyfind(Name, 1, S) of
+        false -> not_sent;
+        {Name, Pid, _, _} -> gen_server:cast(Pid, {From, Msg})
     end,
-    {noreply, S};
+    {reply, R, S0}.
+
 handle_cast(stop, S) -> {stop, normal, S}.
 handle_info(undefined_info, S) -> {noreply, S}.
 terminate(normal, _S) -> ok.
