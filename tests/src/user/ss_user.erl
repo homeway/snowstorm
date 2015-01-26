b5c88ab92079b0a9e0723a1e62f003624c421996
homeway <homeway.xue@gmail.com>
2015-01-26 16:31:18 +0800

    模型信息的访问也在gen_server中提供

--- tests/src/user/ss_user.erl
+++ tests/src/user/ss_user.erl
@@ -3,8 +3,13 @@
 -behaviour(gen_server).
 -export([start_link/0, start_link/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([model/2]).
 -export([create/2, update/2, delete/2, find/2, all/1]).
 -export([hello/1, who/1, login/3, logout/1]).
+
+-define(db, ss_nosqlite).
+-define(m, model_user).
+-define(res, user).
 
 start_link()       -> gen_server:start_link(?MODULE, [], []).
 start_link(UserId) -> gen_server:start_link(?MODULE, [UserId], []).
@@ -24,10 +29,10 @@ hello(#{id:=undefined}=S) -> {reply, #{}, S};
 hello(#{id:=Id}=S) -> find(Id, S).
 who(#{id:=Id}=S) -> {reply, Id, S}.
 
+%% model action
+model(Name, S) -> {reply, ?m:Name(), S}.
+
 %% db action
--define(db, ss_nosqlite).
--define(m, model_user).
--define(res, user).
 create(M, S)   -> {reply, ?db:create(?res, M), S}.
 update(M, S)   -> {reply, ?db:patch (?res, M), S}.
 delete(Key, S) -> {reply, ?db:delete(?res, Key), S}.
