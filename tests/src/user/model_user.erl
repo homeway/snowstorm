96628ff3abeb33d6b96ee2b682ff3abecb1952fb
homeway <homeway.xue@gmail.com>
2015-01-26 16:22:49 +0800

    实现一个通用的handle_call模式，并将数据库操作迁移到gen_server中

--- tests/src/user/model_user.erl
+++ tests/src/user/model_user.erl
@@ -1,13 +1,8 @@
 %% -*- mode: nitrogen -*-
 -module(model_user).
--export([m/1]).
--export([create/1, update/1, delete/1, get/1, all/0]).
--export([check_password/2]).
+-compile(export_all).
 
--define(db, ss_nosqlite).
--define(res, user).
-
-m() -> [
+all() -> ss_model:confirm_model([
     {"类型", #{value=>"普通会员", validate=>[required]}},
     {"账户名", #{value=>"yifan", validate=>[required]}},
     {"性别", #{value=>"女"}},
@@ -19,24 +14,12 @@ m() -> [
     {"头像", #{type=>link, value=>"/themes/cube/img/samples/scarlet-159.png"}},
     {"生日", #{type=>date}},
     {"联系人", #{type=>tags}}
-].
+]).
 
-m(all)      -> ss_model:confirm_model(m());
-m(new)      -> m(all);
-m(index)    -> ss_model:filter(["账户名", "昵称"], m());
-m(edit)     -> ss_model:drop(["账户名", "类型"], m());
-m(show)     -> ss_model:drop(["密码"], m());
-m(password) -> ss_model:filter(["账户名", "密码"], m());
-m(profile)  -> ss_model:filter(["账户名", "昵称", "头像"], m()).
+new()      -> all().
+index()    -> ss_model:filter(["账户名", "昵称"], all()).
+edit()     -> ss_model:drop(["账户名", "类型"], all()).
+show()     -> ss_model:drop(["密码"], all()).
+password() -> ss_model:filter(["账户名", "密码"], all()).
+profile()  -> ss_model:filter(["账户名", "昵称", "头像"], all()).
 
-%% db标准方法
-create(M)   -> ?db:create(?res, M).
-update(M)   -> ?db:patch (?res, M).
-delete(Key) -> ?db:delete(?res, Key).
-get(Key)    -> ?db:get   (?res, Key, m(show)).
-all()       -> ?db:all   (?res, m(all)).
-
-%% db的非标操作也应在此定义
-check_password(UserId, Pass) ->
-    Info = ?MODULE:get(UserId),
-    ss_model:equal("密码", Info, Pass).
--- tests/src/user/ss_user.erl
+++ tests/src/user/ss_user.erl
@@ -3,27 +3,46 @@
 -behaviour(gen_server).
 -export([start_link/0, start_link/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([create/2, update/2, delete/2, find/2, all/1]).
+-export([hello/1, who/1, login/3, logout/1]).
 
 start_link()       -> gen_server:start_link(?MODULE, [], []).
 start_link(UserId) -> gen_server:start_link(?MODULE, [UserId], []).
 
-init(_) -> {ok, #{id=>anonymouse}}.
-
-handle_call(hello, _From, #{id:=anonymouse}=S) ->  {reply, #{}, S};
-handle_call(hello, _From, #{id:=Id}=S) -> {reply, model_user:get(Id), S};
-handle_call(whoami, _From, #{id:=Id}=S) -> {reply, Id, S};
-
-handle_call({login, Id, Pass}, _From, #{id:=anonymouse}=S) ->
-    case model_user:check_password(Id, Pass) of
-        true -> {reply, ok, S#{id=>Id}};
-        false -> {reply, invalid_user_or_pass, S}
-    end;
-
-handle_call({login, _Id, _Pass}, _From, S) -> {reply, already_login, S};
-handle_call(logout, _From, S) -> {reply, ok, S#{id=>anonymouse}}.
-
+init(_) -> {ok, #{id=>undefined}}.
+handle_call(Fun, _From, S) when is_atom(Fun) -> apply(?MODULE, Fun, [S]);
+handle_call([Fun|Args], _From, S) when is_atom(Fun) -> apply(?MODULE, Fun, Args++[S]).
 handle_cast({_Msg, _From}, S) -> {noreply, S}.
-
 handle_info(undefined_info, S) ->  {noreply, S}.
 terminate(normal, _S) -> ok.
 code_change(undefined_oldVsn, S, _Extra) -> {ok, S}.
+
+%% priv -------------------------------------------------
+
+%% helper info
+hello(#{id:=undefined}=S) -> {reply, #{}, S};
+hello(#{id:=Id}=S) -> find(Id, S).
+who(#{id:=Id}=S) -> {reply, Id, S}.
+
+%% db action
+-define(db, ss_nosqlite).
+-define(m, model_user).
+-define(res, user).
+create(M, S)   -> {reply, ?db:create(?res, M), S}.
+update(M, S)   -> {reply, ?db:patch (?res, M), S}.
+delete(Key, S) -> {reply, ?db:delete(?res, Key), S}.
+find(Key, S)   -> {reply, ?db:get   (?res, Key, ?m:show()), S}.
+all(S)         -> {reply, ?db:all   (?res, ?m:all()), S}.
+
+%% login and logout
+login(Id, Pass, #{id:=undefined}=S) ->
+    case check_password(Id, Pass) of
+        true -> {reply, ok, S#{id=>Id}};
+        false -> {reply, invalid_user_or_pass, S}
+    end;
+login(_Id, _Pass, S) -> {reply, already_login, S}.
+logout(S) -> {reply, ok, S#{id=>undefined}}.
+
+check_password(UserId, Pass) ->
+    Info = ?db:get(?res, UserId, ?m:show()),
+    ss_model:equal("密码", Info, Pass).
